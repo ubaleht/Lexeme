c2432881c23d8c86f8c71576c90d747687c36a37
ubaleht <ubaleht@gmail.com>
2022-03-09 17:46:27 +0600

    1. Add two new controllers and views; 2. Modify and add content

---
+++ src/LexemeWebApp/Lexeme/Controllers/DialectsOfSiberianEstoniansController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lexeme.Controllers
+{
+    public class DialectsOfSiberianEstoniansController : Controller
+    {
+        // GET: DialectsOfSiberianEstonians
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/LexemeWebApp/Lexeme/Controllers/SiberianTatarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lexeme.Controllers
+{
+    public class SiberianTatarController : Controller
+    {
+        // GET: SiberianTatar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
