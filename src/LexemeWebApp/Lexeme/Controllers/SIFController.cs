df9ae9de204d509daf35ecfaaf311261939d47ac
ubaleht <ubaleht@gmail.com>
2022-04-03 21:27:03 +0600

    1. Redirect to SiberianIngrianFinnishController. 2. Improved UI.

---
+++ src/LexemeWebApp/Lexeme/Controllers/SIFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lexeme.Controllers
+{
+    public class SIFController : Controller
+    {
+        // GET: SIF
+        public ActionResult Index()
+        {
+            return RedirectPermanent("~/SiberianIngrianFinnish/Index");
+        }
+    }
+}
